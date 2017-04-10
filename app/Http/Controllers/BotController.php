be74f0f100675afd7b8575d7c30d5cfca460f95f
chronolai <x941i6c04@gmail.com>
2017-04-11 01:39:54 +0800

    process line event flow

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Log;
 use LINE\LINEBot;
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\FollowEvent;
+use LINE\LINEBot\Event\UnfollowEvent;
+use LINE\LINEBot\Event\JoinEvent;
+use LINE\LINEBot\Event\LeaveEvent;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\Exception\InvalidEventRequestException;
 use LINE\LINEBot\Exception\InvalidSignatureException;
@@ -18,49 +22,135 @@ use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class BotController extends Controller
 {
+    public function __construct()
+    {
+        $this->client = new CurlHTTPClient(env('CHANNEL_TOKEN'));
+        $this->bot = new LINEBot($this->client, ['channelSecret' => env('CHANNEL_SECRET')]);
+        $this->commands = [[
+                'name' => 'help',
+                'pattern' => '\/help',
+                'description' => 'help',
+            ], [
+                'name' => 'info',
+                'pattern' => '\/info',
+                'description' => 'info',
+            ], [
+                'name' => 'reg',
+                'pattern' => '\/reg\s(\d+)\s(\d+)',
+                'description' => 'reg',
+            ], [
+                'name' => 'unreg',
+                'pattern' => '\/unreg',
+                'description' => 'unreg',
+            ], [
+                'name' => 'reply',
+                'pattern' => '(.*)',
+                'description' => 'reply',
+            ]];
+    }
+
     public function replyMessage(Request $request)
     {
-	    $httpClient = new CurlHTTPClient(env('CHANNEL_TOKEN'));
-	    $bot = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
+        $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
+        if (empty($signature)) {
+            return response('Bad Request', 400);
+        }
 
-	    $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
-	    if (empty($signature)) {
-	        Log::Info('Bad Request');
-	        return response('Bad Request', 400);
-	    }
+        try {
+            $events = $this->bot->parseEventRequest($request->getContent(), $signature);
+        } catch (InvalidSignatureException $e) {
+            return response('Invalid signature', 400);
+        } catch (UnknownEventTypeException $e) {
+            return response('Unknown event type has come', 400);
+        } catch (UnknownMessageTypeException $e) {
+            return response('Unknown message type has come', 400);
+        } catch (InvalidEventRequestException $e) {
+            return response("Invalid event request", 400);
+        }
 
-	    try {
-	        $events = $bot->parseEventRequest($request->getContent(), $signature);
-	    } catch (InvalidSignatureException $e) {
-	        Log::Info('Invalid signature');
-	        return response('Invalid signature', 400);
-	    } catch (UnknownEventTypeException $e) {
-	        Log::Info('Unknown event type has come');
-	        return response('Unknown event type has come', 400);
-	    } catch (UnknownMessageTypeException $e) {
-	        Log::Info('Unknown message type has come');
-	        return response('Unknown message type has come', 400);
-	    } catch (InvalidEventRequestException $e) {
-	        Log::Info("Invalid event request");
-	        return response("Invalid event request", 400);
-	    }
+        foreach ($events as $event) {
+            if (($event instanceof MessageEvent)) {
+                $this->handleMessageEvent($event);
+            }
+            else if (($event instanceof FollowEvent)) {
+                $this->handleFollowEvent($event);
+            }
+            else if (($event instanceof UnfollowEvent)) {
+                $this->handleUnfollowEvent($event);
+            }
+            else if (($event instanceof JoinEvent)) {
+                $this->handleJoinEvent($event);
+            }
+            else if (($event instanceof LeaveEvent)) {
+                $this->handleLeaveEvent($event);
+            }
+            else {
+                Log::Info('Non message event has come');
+                continue;
+            }
+        }
 
-	    foreach ($events as $event) {
-	        if (!($event instanceof MessageEvent)) {
-	            Log::Info('Non message event has come');
-	            continue;
-	        }
+        return response("ok", 200);
+    }
 
-	        if (!($event instanceof TextMessage)) {
-	            Log::Info('Non text message has come');
-	            continue;
-	        }
+    public function processCommands($message='')
+    {
+        foreach ($this->commands as $command) {
+            $output = [];
+            preg_match('/^'.$command['pattern'].'$/', $message, $output);
+            if (count($output) > 0) {
+                $command['source'] = $message;
+                $command['result'] = $output;
+                return $command;
+            }
+        }
+    }
 
-	        $replyText = $event->getText();
-	        $resp = $bot->replyText($event->getReplyToken(), $replyText);
-	        Log::Info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-	    }
+    public function handleMessageEvent($event='')
+    {
+        if (!($event instanceof TextMessage)) {
+            Log::Info('Non text message has come');
+            return;
+        }
 
-	    return response("ok", 200);
+        $command = $this->processCommands($event->getText());
+
+        $user_id = $event->getUserId();
+        $message = sprintf('[%s] %s', $command['name'], $command['description']);
+
+        $resp = $this->bot->replyText($event->getReplyToken(), $message);
+        Log::Info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+    }
+
+    public function handleFollowEvent($event='')
+    {
+        $message = sprintf('follow');
+        $resp = $this->bot->replyText($event->getReplyToken(), $message);
+        Log::Info('handleFollowEvent');
+        Log::Info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+    }
+
+    public function handleUnfollowEvent($event='')
+    {
+        $message = sprintf('unfollow');
+        $resp = $this->bot->replyText($event->getReplyToken(), $message);
+        Log::Info('handleUnfollowEvent');
+        Log::Info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+    }
+
+    public function handleJoinEvent($event='')
+    {
+        $message = sprintf('join');
+        $resp = $this->bot->replyText($event->getReplyToken(), $message);
+        Log::Info('handleJoinEvent');
+        Log::Info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+    }
+
+    public function handleLeaveEvent($event='')
+    {
+        $message = sprintf('leave');
+        $resp = $this->bot->replyText($event->getReplyToken(), $message);
+        Log::Info('handleLeaveEvent');
+        Log::Info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
     }
 }
